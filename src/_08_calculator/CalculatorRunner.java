5859bbb884b0c7800871342cb55f39f0ddbe79f1
League Student <league@iMac-17.attlocal.net>
2019-10-05 12:59:25 -0700

    Calculator (IP)
    :smile:

--- src/_08_calculator/CalculatorRunner.java
+++ src/_08_calculator/CalculatorRunner.java
@@ -3,8 +3,8 @@ package _08_calculator;
 public class CalculatorRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Calculator chenjia = new Calculator();
+		
 	}
 
 }
